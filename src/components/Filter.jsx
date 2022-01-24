08188444ac8a71d91b72872c9a116369ffb698e3
aki2034 <bp18094@shibaura-it.ac.jp>
2022-01-24 11:07:48 +0900

    first commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import 'bulma/css/bulma.css';
+
+export const Filter = (props) => {
+
+  // propsを定義
+  const { value, onChange } = props;
+
+  // フィルターの切り替え
+  const handleClick = (key, event) => {
+    event.preventDefault();
+    onChange(key);
+  };
+
+  return (
+    <div className="panel-tabs">
+      <a
+        href="#"
+        onClick={handleClick.bind(null, 'ALL')}
+        className={classNames({ 'is-active': value === 'ALL' })}
+      >全て</a>
+      <a
+        href="#"
+        onClick={handleClick.bind(null, 'TODO')}
+        className={classNames({ 'is-active': value === 'TODO' })}
+      >やるべき事</a>
+      <a
+        href="#"
+        onClick={handleClick.bind(null, 'DONE')}
+        className={classNames({ 'is-active': value === 'DONE' })}
+      >終わった事</a>
+    </div>
+  );
+}
+
+export default Filter;
