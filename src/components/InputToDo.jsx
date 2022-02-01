e497c1dd235d6a22d082213c2e77c34e25794178
aki2034 <bp18094@shibaura-it.ac.jp>
2022-02-01 19:56:12 +0900

    入力箇所の分割

--- src/components/InputToDo.jsx
+++ src/components/InputToDo.jsx
@@ -4,32 +4,44 @@ import 'bulma/css/bulma.css';
 export const InputToDo = (props) => {
   
     // stateを作成
-    const [text, setText] = useState('');
+    const [Titletext, setTitleText] = useState('');
+    const [Contenttext, setContentText] = useState('');
 
     //入力値をtextに反映
-    const handleChange = e => setText(e.target.value);
+    const handleTitleChange = e => setTitleText(e.target.value);
+    const handleContentChange = e => setContentText(e.target.value);
 
     // Enter押下時、ToDoに追加
     const handleEnter = e => {
       if (e.key === 'Enter') {
-        // 入力値が空白文字の場合終了
-        if (!text.match(/\S/g) ) return;
+          // 入力値が空白文字の場合終了
+          if (!Titletext.match(/\S/g) ) return;
         // ToDoAppクラスの「handleAdd」関数を実行
-        props.onAdd(text);
-        setText('');
+        props.onAdd(Titletext, Contenttext);
+        setContentText('');
       }
     };
-  
+    
     return (
       <div className="panel-block">
+        <label>
+          項目
         <input
           class="input"
           type="text"
-          placeholder="やるべき事を記入せよ！！"
-          value={text}
-          onChange={handleChange}
-          onKeyPress={handleEnter}
+          placeholder="タイトル"  
+          value={Titletext}
+          onChange={handleTitleChange}
         />
+        <input 
+          class="input"
+          type="text"
+          placeholder='内容'
+          value={Contenttext}
+          onChange={handleContentChange}
+          onKeyPress={handleEnter}
+        ></input>
+        </label>
       </div>
     );
   }
